ec9e1d2845b43f57d0ae9ed98d3fe978bc6e9907
junghwan97 <jhzzang97@gmail.com>
2023-03-16 08:14:15 +0900

    연습문제 3.15

--- src/main/java/ch16/sec05/exam03/ConstructorReferenceExample.java
+++ src/main/java/ch16/sec05/exam03/ConstructorReferenceExample.java
@@ -4,11 +4,19 @@ public class ConstructorReferenceExample {
     public static void main(String[] args) {
         Person person = new Person();
 
-        Member m1 = person.getMember1(Member::new);
+//        Member m1 = person.getMember1(Member::new);
+        Member m1 = person.getMember1((id) -> {
+            Member m = new Member(id);
+            return m;
+        });
         System.out.println(m1);
         System.out.println();
 
-        Member m2 = person.getMember2(Member::new);
+//        Member m2 = person.getMember2(Member::new);
+        Member m2 = person.getMember2((id, name) -> {
+            Member m = new Member(id, name);
+            return m;
+        });
         System.out.println(m2);
     }
 }
