4f64311d54baf1e55ef62d04cba5bd6097399fe9
junghwan97 <jhzzang97@gmail.com>
2023-03-22 08:24:15 +0900

    rename 3.21

---
+++ src/main/java/ch11/lecture/p02try_catch/C06MulipleException.java
@@ -0,0 +1,16 @@
+package ch11.lecture.p02try_catch;
+
+public class C06MulipleException {
+    public static void main(String[] args) {
+        try {
+            int[] a = {0, 1};
+            int c = 3 / a[0];
+
+            // ArithmeticException
+            // ArrayIndexOutOfBoundsException
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        System.out.println("continue...");
+    }
+}
